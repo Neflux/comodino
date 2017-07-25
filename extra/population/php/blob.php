34c5304d373f4a2d4ca9401eadd316faf84122da
seicaratteri <pietro.bolcato@studenti.unitn.it>
2017-07-25 16:21:23 +0200

    DB population updarte dio besti

--- extra/population/php/blob.php
+++ extra/population/php/blob.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = new mysqli("localhost", "root", "","devecommerce");
+$conn = new mysqli("localhost", "root", "","ecommerce");
 //$conn = new mysqli("localhost", "root", "toor","ecommerce");
 
 // Check connection
@@ -12,7 +12,7 @@ echo "Connected successfully";
 
 for ($i = 1; $i <= 200; $i++) {
 	$file = file_get_contents('./img/1 ('.strval($i).')');
-	$file = base64_encode($file);
+	//$file = base64_encode($file);
 	$file = $conn->real_escape_string($file);
 	//$file = addslashes($file);
 	if($conn->query("INSERT INTO ShopPhoto (Image, ShopID) VALUES ('$file','$i')") !== TRUE){
