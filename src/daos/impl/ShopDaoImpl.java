4d35c8d71f22238e833bebb4ffad96906fc6cfa5
lucascott <lucascott96@hotmail.it>
2017-10-28 17:20:43 +0200

    Revert "edit shop dao"
    This reverts commit 9ae80be6378a5521498e89d5894dc8000eb2bfbc.

--- src/daos/impl/ShopDaoImpl.java
+++ src/daos/impl/ShopDaoImpl.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class ShopDaoImpl implements ShopDao {
     private Connection con;
 
-    public ShopDaoImpl() {
+    public ShopDaoImpl(Connection con) {
         this.con = DBManager.getCon();
     }
 
